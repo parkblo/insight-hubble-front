53e44742007dbf386697cec880ec2289c9e40b38
Byeongju Park <badmuxi@gmail.com>
2024-03-29 22:13:06 +0900

    ✨ feat: 사이드바 구현

---
+++ src/pages/Discover/Discover.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Discover() {
+    return (
+        <h1>Discover Page</h1>
+    )
+}
+
+export default Discover;
---
+++ src/pages/Post/Post.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Post() {
+    return (
+        <h1>Post Page</h1>
+    )
+}
+
+export default Post;
