236f54d79b56e221af8d57977fd818d6b910bc65
Byeongju Park <badmuxi@gmail.com>
2024-05-04 23:01:35 +0900

    ✨ feat: 포스트 클릭 시 뷰 페이지로 이동

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import { Typography, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 import styles from "./PostItemStyles";
 import PostHeader from "./PostHeader";
 import dummy from "dummydata/dummy.json";
 function PostItem(postObject: any) {
+  const nonstyle = {
+    color: "inherit",
+    textDecoration: "none",
+  };
+
   return (
     // TO DO: postObject props에 실제 데이터 받아오기.
 
@@ -12,10 +18,18 @@ function PostItem(postObject: any) {
       {dummy.map((item: any) => (
         <Box sx={styles.container}>
           <PostHeader item={item} />
-          <Box sx={styles.postBox}>
-            <Typography sx={styles.postBox.title}>{item.title}</Typography>
-            <Typography sx={styles.postBox.content}>{item.content}</Typography>
-          </Box>
+          <Link
+            style={nonstyle}
+            to={`/view/${item.post_id}`}
+            key={item.post_id}
+          >
+            <Box sx={styles.postBox}>
+              <Typography sx={styles.postBox.title}>{item.title}</Typography>
+              <Typography sx={styles.postBox.content}>
+                {item.content}
+              </Typography>
+            </Box>
+          </Link>
         </Box>
       ))}
     </Box>
