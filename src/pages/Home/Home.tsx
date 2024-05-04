45f5f1d5d9b1a0fe3fbd994e7ffd79e8109b1bcc
Byeongju Park <badmuxi@gmail.com>
2024-05-04 16:32:49 +0900

    ✨ feat: home.tsx useEffect 적용

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@mui/material";
+import axios from "axios";
 
 import MenuTitle from "components/MenuTitle/MenuTitle";
 import CategoryTitle from "components/MenuTitle/CategoryTitle";
@@ -8,6 +9,31 @@ import RemindList from "components/HomeItem/RemindList";
 import SummaryList from "components/HomeItem/SummaryList";
 
 function Home() {
+  const [data, setData] = React.useState({
+    state: false,
+    monthly_posts: 0,
+    solved_question: 0,
+    unsolved_question: 0,
+    postsWithin30days: [],
+    postsMoreThan30days: [],
+  });
+
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/");
+      // 성공 로직
+      console.log(response.data);
+      setData({ state: true, ...response.data });
+    } catch (error: any) {
+      // 실패 로직
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Box>
       <MenuTitle title="Home" />
